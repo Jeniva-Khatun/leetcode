43a65d20a72036fb79035cf88db343c82674c8a4
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-07-29 00:25:20 +0530

    implement binary search

---
+++ 35_search_insert_position.c
@@ -0,0 +1,18 @@
+int searchInsert(int* nums, int numsSize, int target) {
+  int high=numsSize-1,low=0,mid;
+  while (low<=high){
+    mid=low+(high-low)/2;
+  if (nums[mid]==target){
+    return mid ;
+  } 
+  else if (nums[mid]<target){
+    low=mid+1;
+  }
+  else {
+    high=mid-1;
+  }
+  }
+  return low;
+
+  
+}
