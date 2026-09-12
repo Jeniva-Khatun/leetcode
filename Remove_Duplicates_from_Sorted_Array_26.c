3452f7fafe3771d94aed29c03ee843a3a3a0d77a
Jeniva Khatun <khatunjeniva11@gmail.com>
2026-09-12 23:49:59 +0530

    remove duplicates from sorted array using two pointer

---
+++ Remove_Duplicates_from_Sorted_Array_26.c
@@ -0,0 +1,16 @@
+int removeDuplicates(int* nums, int numsSize) {
+ 
+
+    int i = 0;  // low pointer
+
+    for (int j = 1; j < numsSize; j++) {  // high pointer
+
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+
+}
